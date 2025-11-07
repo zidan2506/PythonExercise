137f342e7eac2cfdf76e99860c7ba4b340b51c46
zidan2506 <duydan@metropolia.fi>
2025-11-08 05:08:21 +0700

    Module 12

--- Exercise  12/Exercise 12.2.py
+++ Exercise  12/Exercise 12.2.py
@@ -1,16 +1,26 @@
 import requests
 
 r = input('Enter the name of a municipality: ')
-request = 'http://api.weatherapi.com/v1/current.json?key=2ce58d6386004c7d872210654250611&q=' + r
-try:
-    response = requests.get(request)
-    if response.status_code == 200:
-        res_json = response.json()
-        k_temp = (res_json['current']['temp_f'] - 32)*(5/9)+273.1
-        c_temp = k_temp - 273.15
+cityname = False
+while not cityname:
+    request = f'https://api.openweathermap.org/data/2.5/weather?q={r}&APPID=3abf1b6e6194cd59053b48a53ecdce2a'
+    try:
+        
+        response = requests.get(request)
+        if response.status_code == 200:
+            res_json = response.json()
+            if  res_json['cod'] == 200:
+                k_temp = res_json['main']['temp'] 
+                c_temp = k_temp - 273.15
+                cityname = True
+                print(f'Weather condition: {res_json['weather'][0]['main']}\nCurrent temperature: {c_temp:.1f}°C')
 
-        print(f'Weather condition: {res_json['current']['condition']['text']}\nCurrent temperature: {c_temp:.1f}°C')
-
-except requests.exceptions.RequestException as e:
-    print("Request could not be completed")
-    print(e.args)
+        else:
+            print(f'{response.json()['message']}\nPlease enter the correct city name')
+        
+        
+            
+        
+    except requests.exceptions.RequestException as e:
+        print("Request could not be completed")
+        print(e.args)
